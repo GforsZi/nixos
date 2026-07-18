b602d5d8d0db2025364d49b4b071a02790e03dca
GforsZi <givaldigumelarsetiawan@gmail.com>
2026-07-18 17:54:18 +0700

    restructured my shred directory

--- home/profiles/tbook.nix
+++ home/profiles/tbook.nix
@@ -2,8 +2,31 @@
 
 {
   imports = [
-    ../shared
-    ../gui
+  ../shared/apps/spotify.nix
+  ../shared/apps/vesktop.nix
+  ../shared/apps/alsa-tools.nix
+  ../shared/apps/browsers.nix
+  ../shared/apps/tor-browser.nix
+  ../shared/apps/mpv.nix
+  ../shared/apps/obs.nix
+
+  ../shared/shell/tmux.nix
+  ../shared/shell/zsh.nix
+
+  ../shared/terminal/kitty.nix
+  
+  ../shared/cli_tools/opencode.nix
+  ../shared/cli_tools/yazi.nix
+  ../shared/cli_tools/ffmpeg.nix
+
+  ../shared/dev/direnv.nix
+  ../shared/dev/gcc.nix
+  ../shared/dev/git.nix
+  ../shared/dev/nodejs.nix
+  ../shared/dev/php.nix
+  ../shared/dev/go.nix
+
+  ../shared/editor/neovim.nix
   ];
 
   home.stateVersion = "26.05";
